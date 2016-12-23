75b16833dd86ca89fc72235f9f8e4779c865d4bb
Andreas Watterott <aw@watterott.net>
2016-12-23 22:37:55 +0100

    updated USB ids

---
+++ software/bootloader/README.md
@@ -0,0 +1,2 @@
+# SAM-BAR
+SAM-BAR source code and project files for [Atmel Studio 7](http://www.atmel.com/tools/atmelstudio.aspx).
--- software/bootloader/src/config/conf_board.h
+++ software/bootloader/src/config/conf_board.h
@@ -16,7 +16,7 @@
 #if defined BOARD_MKR1000         /* Arduino MKR1000 */
 
 #define USB_DEVICE_VENDOR_ID         0x1D50 // Arduino = 0x2341
-#define USB_DEVICE_PRODUCT_ID        0x6110 // Arduino MKR1000 = 0x004E, Genuino MKR1000 = 0x024E
+#define USB_DEVICE_PRODUCT_ID        0x6112 // Arduino MKR1000 = 0x004E, Genuino MKR1000 = 0x024E
 #define USB_DEVICE_MANUFACTURE_NAME  "Arduino"
 #define USB_DEVICE_PRODUCT_NAME      "Arduino MKR1000"
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
@@ -32,8 +32,8 @@
 
 #elif defined BOARD_EITECH        /* Eitech Robotics */
 
-#define USB_DEVICE_VENDOR_ID         0x1D50
-#define USB_DEVICE_PRODUCT_ID        0x6110
+#define USB_DEVICE_VENDOR_ID         0x1D50 // OpenMoko = 0x1D50
+#define USB_DEVICE_PRODUCT_ID        0x6110 // Eitech Robotics Bootloader = 0x6110
 #define USB_DEVICE_MANUFACTURE_NAME  "Eitech"
 #define USB_DEVICE_PRODUCT_NAME      "Eitech Robotics"
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
@@ -53,8 +53,8 @@
 
 #else                             /* no board defined */
 
-#define USB_DEVICE_VENDOR_ID         0x1D50
-#define USB_DEVICE_PRODUCT_ID        0x6110
+#define USB_DEVICE_VENDOR_ID         0x1D50 // OpenMoko = 0x1D50
+#define USB_DEVICE_PRODUCT_ID        0x6112 // SAM-BAR Bootloader = 0x6112
 #define USB_DEVICE_MANUFACTURE_NAME  "Watterott"
 #define USB_DEVICE_PRODUCT_NAME      "SAM-BAR"
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
