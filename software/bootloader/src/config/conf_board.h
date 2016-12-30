793dd2952a12c13cac0af8ec501e063ff187487a
Andreas Watterott <aw@watterott.net>
2016-12-31 00:20:07 +0100

    added more boards

--- software/bootloader/src/config/conf_board.h
+++ software/bootloader/src/config/conf_board.h
@@ -10,24 +10,45 @@
 
 
 //#define BOARD_MKR1000
+//#define BOARD_MKRZERO
+//#define BOARD_ZERO
 //#define BOARD_EITECH
 
 
 #if defined BOARD_MKR1000         /* Arduino MKR1000 */
 
 #define USB_DEVICE_VENDOR_ID         0x1D50 // Arduino = 0x2341
-#define USB_DEVICE_PRODUCT_ID        0x6112 // Arduino MKR1000 = 0x004E, Genuino MKR1000 = 0x024E
+#define USB_DEVICE_PRODUCT_ID        0x6112 // MKR1000 Bootloader = 0x004E
 #define USB_DEVICE_MANUFACTURE_NAME  "Arduino"
 #define USB_DEVICE_PRODUCT_NAME      "Arduino MKR1000"
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
 #define VOLUMELABEL                  "BOOTLOADER"
-
 #define LED_BOOT                     PORT_PA20  // PA20 (digital pin 6)
 #define LED_BOOT_GROUP               0          // Port A
-// inline functions
-#define LED_BOOT_INIT()              PORT->Group[LED_BOOT_GROUP].DIRSET.reg = LED_BOOT
-#define LED_BOOT_ON()                PORT->Group[LED_BOOT_GROUP].OUTSET.reg = LED_BOOT
-#define LED_BOOT_OFF()               PORT->Group[LED_BOOT_GROUP].OUTCLR.reg = LED_BOOT
+
+
+#elif defined BOARD_MKRZERO       /* Arduino MKRZero */
+
+#define USB_DEVICE_VENDOR_ID         0x1D50 // Arduino = 0x2341
+#define USB_DEVICE_PRODUCT_ID        0x6112 // MKRZero Bootloader = 0x004F
+#define USB_DEVICE_MANUFACTURE_NAME  "Arduino"
+#define USB_DEVICE_PRODUCT_NAME      "Arduino MKRZero"
+#define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
+#define VOLUMELABEL                  "BOOTLOADER"
+#define LED_BOOT                     PORT_PB08  // PB08 (digital pin 32)
+#define LED_BOOT_GROUP               1          // Port B
+
+
+#elif defined BOARD_ZERO          /* Arduino Zero */
+
+#define USB_DEVICE_VENDOR_ID         0x1D50 // Arduino = 0x2341
+#define USB_DEVICE_PRODUCT_ID        0x6112 // Zero Bootloader = 0x004D
+#define USB_DEVICE_MANUFACTURE_NAME  "Arduino"
+#define USB_DEVICE_PRODUCT_NAME      "Arduino Zero"
+#define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
+#define VOLUMELABEL                  "BOOTLOADER"
+#define LED_BOOT                     PORT_PA17  // PA17 (digital pin 13)
+#define LED_BOOT_GROUP               0          // Port A
 
 
 #elif defined BOARD_EITECH        /* Eitech Robotics */
@@ -38,17 +59,12 @@
 #define USB_DEVICE_PRODUCT_NAME      "Eitech Robotics"
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
 #define VOLUMELABEL                  "BOOTLOADER"
-
 #ifdef DEBUG
 # define LED_BOOT                    PORT_PA02  // PA02/J1
 #else
 # define LED_BOOT                    PORT_PA31  // PA31/SWDIO
 #endif  
 #define LED_BOOT_GROUP               0          // Port A
-// inline functions
-#define LED_BOOT_INIT()              PORT->Group[LED_BOOT_GROUP].DIRSET.reg = LED_BOOT
-#define LED_BOOT_ON()                PORT->Group[LED_BOOT_GROUP].OUTSET.reg = LED_BOOT
-#define LED_BOOT_OFF()               PORT->Group[LED_BOOT_GROUP].OUTCLR.reg = LED_BOOT
 
 
 #else                             /* no board defined */
@@ -60,6 +76,13 @@
 #define USB_DEVICE_SERIAL_NAME       "876427839026" // Disk SN for MSC
 #define VOLUMELABEL                  "BOOTLOADER"
 
+#endif
+
+
+#ifdef LED_BOOT
+# define LED_BOOT_INIT()              PORT->Group[LED_BOOT_GROUP].DIRSET.reg = LED_BOOT
+# define LED_BOOT_ON()                PORT->Group[LED_BOOT_GROUP].OUTSET.reg = LED_BOOT
+# define LED_BOOT_OFF()               PORT->Group[LED_BOOT_GROUP].OUTCLR.reg = LED_BOOT
 #endif
 
 
